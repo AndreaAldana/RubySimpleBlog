c1fc1d047447d74150f7a1cdf3871fa258401cfc
AndreaAldana <andrealdanapaz@gmail.com>
2020-11-11 14:16:47 -0300

    Blog terminado confirmacion aceptada

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+    http_basic_authenticate_with name: "Andrea", password: "1234", only: [:destroy]
+
 
     def create
         @post = Post.find(params[:post_id])
@@ -8,5 +10,12 @@ class CommentsController < ApplicationController
 
     private def comment_params
         params.require(:comment).permit(:username, :body)
+    end
+
+    def destroy 
+        @post = Post.find(params[:id])
+        @comment = @post.comments.find(params[:id])
+        @comment.destroy
+        redirect_to post_path(@post)
     end
 end
