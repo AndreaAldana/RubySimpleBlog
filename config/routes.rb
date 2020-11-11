d6aed918f185b95006801ee9fff8c0312625f90c
AndreaAldana <andrealdanapaz@gmail.com>
2020-11-11 13:49:07 -0300

    Blog avanzado

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
-  root 'posts#index'
-  get 'about' => 'pages#about'
+  root 'posts#index', as: 'home'
+  get 'about' => 'pages#about', as: 'About'
 
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
